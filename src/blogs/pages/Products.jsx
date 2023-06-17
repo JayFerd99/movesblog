eba4dd3f1d05ca76c31f1fec3414bc4e158e518b
JayFerd99 <ferdinardking99@gmail.com>
2023-06-17 17:24:09 +0100

    main push 1

--- src/blogs/pages/Products.jsx
+++ src/blogs/pages/Products.jsx
@@ -1,31 +1,241 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Footer from "./Footer";
 import "../products.css";
 import ReactStars from "react-rating-stars-component";
 import { Input, Space, Spin, Button } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
+import product1 from "../../assets/products1.png";
+import product2 from "../../assets/products2.png";
+import product3 from "../../assets/products3.png";
+import product4 from "../../assets/products4.png";
+import product5 from "../../assets/products5.png";
+import product6 from "../../assets/products6.png";
+import product7 from "../../assets/products7.png";
+import product8 from "../../assets/proudcts8.png";
+import product9 from "../../assets/products9.png";
+import product10 from "../../assets/product10.png";
+import product11 from "../../assets/product11.png";
+import product12 from "../../assets/product12.png";
+import product13 from "../../assets/product13.png";
+import product14 from "../../assets/product14.png";
+import product15 from "../../assets/product15.png";
+import product16 from "../../assets/products16.png";
+import product17 from "../../assets/product17.png";
+import product18 from "../../assets/products18.png";
+import product19 from "../../assets/products19.png";
+import product20 from "../../assets/products20.png";
+import product21 from "../../assets/products21.png";
+import product22 from "../../assets/products22.png";
+import product23 from "../../assets/products23.png";
+import product24 from "../../assets/products24.png";
 
 function Products() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [records, setRecords] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchValue, setSearchValue] = useState("");
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("http://dummyjson.com/products")
-      .then((res) => {
-        setProducts(res.data.products);
-        setRecords(res.data.products);
-        setLoading(false);
-        setFilteredProducts(res.data.products);
-      })
-      .catch((err) => {
-        setError(err); // Set the error state
-        setLoading(false);
-      });
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      const mockProducts = [
+        {
+          id: 1,
+          title: "Product 1",
+          image: product1,
+          rating: 4,
+          description: "Description of Product 1",
+        },
+
+        {
+          id: 2,
+          title: "Product 2",
+          image: product2,
+          rating: 3.5,
+          description: "Description of Product 2",
+        },
+
+        {
+          id: 3,
+          title: "Product 3",
+          image: product3,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 4,
+          title: "Product 4",
+          image: product4,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 5,
+          title: "Product 5",
+          image: product5,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 6,
+          title: "Product 6",
+          image: product6,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 7,
+          title: "Product 7",
+          image: product7,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 8,
+          title: "Product 1",
+          image: product8,
+          rating: 4,
+          description: "Description of Product 1",
+        },
+        {
+          id: 9,
+          title: "Product 2",
+          image: product9,
+          rating: 3.5,
+          description: "Description of Product 2",
+        },
+
+        {
+          id: 10,
+          title: "Product 3",
+          image: product10,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 11,
+          title: "Product 4",
+          image: product11,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 12,
+          title: "Product 5",
+          image: product12,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 13,
+          title: "Product 6",
+          image: product13,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 14,
+          title: "Product 7",
+          image: product14,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 15,
+          title: "Product 1",
+          image: product15,
+          rating: 4,
+          description: "Description of Product 1",
+        },
+        {
+          id: 16,
+          title: "Product 2",
+          image: product16,
+          rating: 3.5,
+          description: "Description of Product 2",
+        },
+
+        {
+          id: 17,
+          title: "Product 3",
+          image: product17,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 18,
+          title: "Product 4",
+          image: product18,
+          rating: 3,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 19,
+          title: "Product 5",
+          image: product19,
+          rating: 4,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 20,
+          title: "Product 6",
+          image: product20,
+          rating: 2.8,
+          description: "Description of Product 3",
+        },
+
+        {
+          id: 21,
+          title: "Product 7",
+          image: product21,
+          rating: 2,
+          description: "Description of Product 3",
+        },
+        {
+          id: 22,
+          title: "Product 1",
+          image: product22,
+          rating: 4,
+          description: "Description of Product 1",
+        },
+
+        {
+          id: 23,
+          title: "Product 2",
+          image: product23,
+          rating: 3.5,
+          description: "Description of Product 2",
+        },
+
+        {
+          id: 24,
+          title: "Product 24",
+          image: product24,
+          rating: 5,
+          description: "Description of Product 3",
+        },
+      ];
+
+      setProducts(mockProducts);
+      setFilteredProducts(mockProducts);
+      setLoading(false);
+    };
+
+    fetchData();
   }, []);
 
   const handleProductClick = (productId) => {
@@ -33,9 +243,10 @@ function Products() {
   };
 
   const getInputData = (event) => {
+    setSearchValue(event.target.value);
     setFilteredProducts(
-      records.filter((r) =>
-        r.title.toLowerCase().includes(event.target.value.toLowerCase())
+      products.filter((product) =>
+        product.title.toLowerCase().includes(event.target.value.toLowerCase())
       )
     );
   };
@@ -48,10 +259,6 @@ function Products() {
     );
   };
 
-  if (error) {
-    return <div>Error: {error.message}</div>; // Display the error message
-  }
-
   return (
     <div className="product-container">
       <h2>Product Reviews</h2>
@@ -62,6 +269,7 @@ function Products() {
           Search
         </Button>
       </Space>
+
       <div className="products">
         {loading ? (
           <div className="Loading">
@@ -72,14 +280,14 @@ function Products() {
             <h3>No products found</h3>
           </div>
         ) : (
-          filteredProducts.map((product, index) => (
+          filteredProducts.map((product) => (
             <div
-              key={index}
+              key={product.id}
               className="product"
               onClick={() => handleProductClick(product.id)}
             >
               <h3>{product.title}</h3>
-              <img src={product.thumbnail} alt={product.title} />
+              <img src={product.image} alt={product.title} />
               <ReactStars
                 name={`rating-${product.id}`}
                 value={product.rating}
@@ -90,6 +298,7 @@ function Products() {
           ))
         )}
       </div>
+
       <Footer />
     </div>
   );
